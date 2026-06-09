3fd749c7269aaf3ab23480eb339a520be44f8a06
Joseph Cooper <kcooper.edu@gmail.com>
2026-06-08 19:02:45 -0600

    Update HelloServlet.java

--- libreria-servlet/src/main/java/hn/uth/libreriaservlet/HelloServlet.java
+++ libreria-servlet/src/main/java/hn/uth/libreriaservlet/HelloServlet.java
@@ -11,7 +11,7 @@ public class HelloServlet extends HttpServlet {
 
     public void init() {
         message = "Hola Alumnos!";
-        
+
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
